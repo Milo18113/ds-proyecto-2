0c36d7fb2374dcffdd1bbeb90beaa30837b07c91
Emilio Puga <milo18113@gmail.com>
2026-02-21 14:05:30 -0500

    feat: Crear Banking Facade para manejar dbs mediante dtos. Actualizar excepciones y entidades para este funcionamiento. Crear LedgerEntryFactory

--- app/domain/entities/customer.py
+++ app/domain/entities/customer.py
@@ -1,9 +1,15 @@
 from dataclasses import dataclass
+import email
+import uuid
+
+from numpy import isin
+
+from app.application.dtos import CustomerCreate, CustomerResponse, CustomerUpdate
 
 
 @dataclass
 class Customer:
-    id: str
+    id: str  # = str(uuid.uuid4())
     name: str
     email: str
     status: str = "ACTIVE"
@@ -13,3 +19,18 @@ class Customer:
             raise ValueError("Customer name cannot be empty")
         if not self.email:
             raise ValueError("Customer email cannot be empty")
+
+    @staticmethod
+    def create_from_dto(dto: CustomerCreate | CustomerResponse):
+        return Customer(
+            id = uuid.uuid4() if isinstance(dto, CustomerCreate) else dto.id,
+            name=dto.name,
+            email=dto.email,
+            status=dto.status
+        )
+
+    def update_from_dto(self, dto: CustomerUpdate):
+        if dto.name is not None:  
+            self.name = dto.name
+        if dto.email is not None: 
+            self.email = dto.email
